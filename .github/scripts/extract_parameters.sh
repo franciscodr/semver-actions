09280985a585578a567e7afbef60fec8a0ab7007
franciscodr <francisco.d@47deg.com>
2023-05-19 14:51:59 +0200

    Fix wrong variable assignment

--- .github/scripts/extract_parameters.sh
+++ .github/scripts/extract_parameters.sh
@@ -6,5 +6,5 @@ then
   echo "scope=$(echo "$1" | sed 's/alpha-\(.*\)/\1/')" >> "$GITHUB_OUTPUT"
 else
   echo "stage=$1" >> "$GITHUB_OUTPUT"
-  echo "stage=auto" >> "$GITHUB_OUTPUT"
+  echo "scope=auto" >> "$GITHUB_OUTPUT"
 fi
